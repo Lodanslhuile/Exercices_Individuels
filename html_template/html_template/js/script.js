0cd28cae850432407e452c0adf433c529c666bd3
LODANSLHUILE\loren <lorenacamposrosas@gmail.com>
2022-10-21 11:59:53 +0200

    2022-10-21_partie_2

--- html_template/html_template/js/script.js
+++ html_template/html_template/js/script.js
@@ -1,21 +1,22 @@
-const message = '\ud83d\udc4b Bonjour'
-var prenom = window.prompt('quel est ton pr\351nom?')
-var naissance = Number(window.prompt('quelle est ton année de naissance?'));
+function askName() {
+    
+    const message = '\ud83d\udc4b Bonjour'
+    var prenom = window.prompt('quel est ton pr\351nom?')
 
-document.body.innerHTML += '<h2>' + message + ' ' + prenom + '</h2>';
-var aujd = new Date();
-var year = aujd.getFullYear();
-
-function AgeCal(year,naissance) {
-    return year - naissance;
+    document.body.innerHTML += '<h2>' + message + ' ' + prenom + '</h2>';
 }
 
-var age = AgeCal(year, naissance)
+function askBirthYear() {
+    var naissance = Number(window.prompt('quelle est ton ann\351e de naissance?'));
+    
+    var aujd = new Date();
+    var year = aujd.getFullYear();
+    var age = year - naissance
 
-document.body.innerHTML += '<h3> Vous avez ' + age + ' ans</h3>';
+    document.body.innerHTML += '<h3> Vous avez ' + age + ' ans</h3>';
+}
+
+askName()
+askBirthYear()
 
 
-console.log(message + prenom)
-console.log(naissance)
-console.log(year)
-console.log(age)
