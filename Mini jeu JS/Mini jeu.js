ee2082797fb3678218eedd24215ae91d8d74b164
LODANSLHUILE\loren <lorenacamposrosas@gmail.com>
2022-10-27 11:33:13 +0200

    2022-10-27_Mini jeu JS etape 3

--- Mini jeu JS/Mini jeu.js
+++ Mini jeu JS/Mini jeu.js
@@ -2,24 +2,42 @@ function EnterNumber() {
    var EnterNumber = window.prompt("Entrer un nombre");
    return EnterNumber // penser à return pour pouvoir utiliser la valeur en dehors de la fonction
 }
-let givenNumber = EnterNumber()
 
-console.log(userNumber)
 
-function didIWin(givenNumber){ //le parametre givenNumber ne fait pas référence à la variable givenNumber déclarée précedemment. On aurait pu appeler la variable précédente userNumber par exemple
-    let result
+function didIWin(givenNumber){ 
+    let message
+    let result = false // on définit une variable false par défaut pour pouvoir l'utiliser plus tard.
 
     if (givenNumber == 22){
-        result = "Bravo ! Vous avez deviné le nombre"
-       
+        message = "Bravo ! Vous avez deviné le nombre"
+        result = true // ici on explique que dans ce cas le resultat est true vs les autres resultats qui sont false.
+    
     } else if (givenNumber < 22){
-        result = "Plus grand!"
+        message = "Plus grand!"
         
     } else if (givenNumber > 22){
-        result = "Plus petit!"
-
+        message = "Plus petit!"
+        
     }
-    alert(result)
+    alert(message)
+
+    return result // on fait un return pour pouvoir récupérer le resultat et l'utiliser en dehors de la fonction.
 }
 
-didIWin(givenNumber) //ici givenNumber fait référence à la variable déclarée ligne 5. Lorsqu'on appelle la fonction elle comprend que le parametre à utiliser c'est celui de la ligne 5 et ça déroule le reste.  
+function gameplay(){
+   
+    let result2 = false // on définit une nouvelle variable qui sera utilisée pour assigner pus tard le resultat true/false de la fonction didIwin.
+    
+    do {
+        let userNumber = EnterNumber()
+        result2 = didIWin (userNumber) // attention à ne pas écrire "let results2" sinon ne comprend pas qu'il s'agit de la variable déclarée ligne 29. 
+    }
+    while (result2 == false) // refaire la boucle tant que le résultat est false.
+
+}
+
+gameplay()
+
+
+
+
