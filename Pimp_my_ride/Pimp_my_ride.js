9ccc28770f446ee8be1ef60ad00de0d029fccca3
Lorena <lorenacamposrosas@gmail.com>
2022-11-15 12:10:39 +0100

    etape 1 Parsing Clean

--- Pimp_my_ride/Pimp_my_ride.js
+++ Pimp_my_ride/Pimp_my_ride.js
@@ -1,29 +1,27 @@
 let tripToParse = "Perdita 8 10 8"
-//manipuler la chaine de caractère pour la trasnformer en un objet
 
 function parseTrip(trip) {
 	const tripArray = tripToParse.split(" ");
-	return tripArray
+	//chaîne de caractère de la ligne 1 transformée en array et stockée dans la variable tripArray
+	
+	let categories = ['client', 'start', 'duration', 'price'];
+	
+	let tripObj = {};
+	
+	categories.forEach((element,index) => {
+		tripObj[element] = tripArray[index];
+		});
+	// https://bobbyhadz.com/blog/javascript-create-object-from-two-arrays
 }
 
 console.log(parseTrip(tripToParse))
 
-let categories = ['client', 'start', 'duration', 'price'];
 
-let tripToArray = parseTrip(tripToParse);
-//chaîne de caractère de la ligne 1 transformée en array et stockée dans la variable tripToArray
 
-let tripObj = {};
 
-categories.forEach((element,index) => {
-tripObj[element] = tripToArray[index];
-});
-// https://bobbyhadz.com/blog/javascript-create-object-from-two-arrays
 
-// categories.forEach(function(element,index){
-// 	tripObj[element] = tripToArray[index];
-// 	return tripObj
-// });
-//tentative de transformation de fonction fléchée en fonction anonyme
 
-console.log(tripObj)
+
+
+
+
