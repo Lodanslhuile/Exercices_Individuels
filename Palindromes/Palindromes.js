e9e950a282b8dc2faa7314dbc5700ca4e6a3272c
Lorena <lorenacamposrosas@gmail.com>
2022-11-24 10:49:00 +0100

    étape 2

--- Palindromes/Palindromes.js
+++ Palindromes/Palindromes.js
@@ -1,6 +1,7 @@
 //Etape 1
 
 //ressources https://codes-sources.commentcamarche.net/source/15737-verifier-une-date et https://bobbyhadz.com/blog/javascript-date-validation-dd-mm-yyyy#:~:text=To%20validate%20a%20date%20that,the%20string%20is%20formatted%20correctly.
+let dateToTest = '23/04/2023'
 
 function isValidDate(d) {
 	e = new RegExp("^[0-9]{2}\/[0-9]{2}\/([0-9]{4})$");
@@ -26,6 +27,31 @@ function isValidDate(d) {
 	return ( m >= 1 && m <=12 && j >= 1 && j <= nbJours[m-1] );
 }
 
-  console.log(isValidDate('03/02/2024')); 
-  console.log(isValidDate('35/01/2022')); 
-  console.log(isValidDate('hello')); 
+console.log(isValidDate(dateToTest)); 
+
+//Etape 2
+
+let testedDate = isValidDate(dateToTest)
+
+function isPalindrome(date){
+    //https://askcodez.com/palindrome-verification-en-javascript.html 
+    //https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+
+	// transforme ma chaine de caractère qui contient des / en une chaine de caractère sans /.
+	joinedDate = date.split('/').join('');
+
+	//transforme joinedDate en array
+	joinedDateArray = joinedDate.split('');
+	
+	//retourne l'array dans l'autre sens puis le ressoude pour former une chaîne de caractère
+	reversedDate= joinedDateArray.reverse().join('');
+
+    //The logical AND (&&) operator and if...else statements in JavaScript https://www.freecodecamp.org/news/javascript-if-else-and-if-then-js-conditional-statements/
+	if (testedDate == true && joinedDate==reversedDate){
+		return true
+    }else{
+		return false
+	}
+}
+
+console.log(isPalindrome(dateToTest))
